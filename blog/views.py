7e831f7be55aaab4612a42bdeea8983e4898f972
kimbellyf <kimbelly_emanuelle@hotmail.com>
2019-06-17 17:22:01 -0300

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
+#Views vai extrair informações do model que você criou e entregá-las a um template
 from django.shortcuts import render
 
 # Create your views here.
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
